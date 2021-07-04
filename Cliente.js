bb1306052b24fdd9f87accab03a61c5b274a3d47
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-07-04 18:54:31 -0300

    better encapsulated account class

--- Cliente.js
+++ Cliente.js
@@ -1,6 +1,4 @@
 export class Cliente {
   nome;
   cpf;
-  agencia;
-  saldo;
 }
--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,7 +1,23 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
   agencia;
-  cliente;
+  _cliente;
   _saldo = 0;
+
+  set cliente(cliente) {
+    if(cliente instanceof Cliente) {
+      this._cliente = cliente;
+    }
+  }
+
+  get cliente() {
+    return this._cliente;
+  }
+
+  get saldo() {
+    return this._saldo;
+  }
 
   sacar(valor) {
     if(this._saldo >= valor) {
